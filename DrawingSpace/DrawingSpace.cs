4227b8d82dca3405929d72d79a1408f46470f5b2
Ahmed Félix <ahmed.felix@gmail.com>
2012-12-02 19:05:22 -0800

    Initial commit

---
+++ DrawingSpace/DrawingSpace.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace DrawingSpace
+{
+    static class DrawingSpace
+    {
+
+        /// <summary>
+        /// Adds an entity to the Model space or current layout.
+        /// </summary>
+        /// <param name="entity"></param>
+        public static void AddEntity(Entity entity)
+        {
+            Database database = HostApplicationServices.WorkingDatabase;
+            Transaction transaction = database.TransactionManager.StartTransaction();
+            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId, OpenMode.ForRead);
+            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+
+            try
+            {
+                record.AppendEntity(entity);
+                transaction.AddNewlyCreatedDBObject(entity, true);
+                transaction.Commit();
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                transaction.Dispose();
+                entity.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Adds an entity to the Model space or current layout, keeping the transaction open
+        /// so the entity can continue being used.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="transaction"></param>
+        public static void AddEntity(Entity entity, Transaction transaction)
+        {
+            Database database = HostApplicationServices.WorkingDatabase;
+            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId, OpenMode.ForRead);
+            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+
+            try
+            {
+                record.AppendEntity(entity);
+                transaction.AddNewlyCreatedDBObject(entity, true);
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Prompts the user to select a single entity from the drawing.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="transaction"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public static Entity GetEntity(String prompt, Transaction transaction, ref PromptStatus status)
+        {
+            PromptEntityOptions options = new PromptEntityOptions(System.Environment.NewLine + prompt);
+            PromptEntityResult result;
+            Entity entity = null;
+            Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            options.AllowNone = true;
+            result = command.GetEntity(options);
+
+            if (result.Status == PromptStatus.OK)
+            {
+                entity = (Entity)transaction.GetObject(result.ObjectId, OpenMode.ForWrite);
+                status = result.Status;
+            }
+
+            return entity;
+        }
+
+        /// <summary>
+        /// Prompts the user to do a selection of one or more objects.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="transaction"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public static DBObjectCollection GetSelection(String prompt, Transaction transaction, ref PromptStatus status)
+        {
+            PromptSelectionOptions options = new PromptSelectionOptions();
+            PromptSelectionResult result;
+            SelectionSet selection;
+            Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
+            DBObjectCollection objectsSelected = new DBObjectCollection();
+
+            options.MessageForAdding = System.Environment.NewLine + prompt;
+            result = command.GetSelection(options);
+            status = result.Status;
+
+            ObjectId[] idArray;
+            DBObject dbObject;
+
+            selection = result.Value;
+
+            if (selection != null)
+            {
+                idArray = selection.GetObjectIds();
+
+                foreach (ObjectId id in idArray)
+                {
+                    dbObject = transaction.GetObject(id, OpenMode.ForWrite, false, true);
+                    objectsSelected.Add(dbObject);
+                }
+            }
+
+            return objectsSelected;
+        }
+    }
+}
---
+++ DrawingSpace/Samples.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace DrawingSpace
+{
+    public class Samples
+    {
+        [CommandMethod("DSADDENTITY")]
+        public static void AddEntity()
+        {
+            Line line = new Line(new Point3d(0, 0, 0), new Point3d(1, 1, 0));
+            DrawingSpace.AddEntity(line);
+        }
+
+        [CommandMethod("DSADDENTITYTRANS")]
+        public static void AddEntityWithTransaction()
+        {
+            Transaction transaction = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction();
+            Line line = new Line(new Point3d(0, 0, 0), new Point3d(1, 1, 0));
+            DrawingSpace.AddEntity(line, transaction);
+
+            // Since the transaction is still open we can continue to manipulate the object
+            // and get access to properties that are set only after adding it to the database.
+            line.Color = Color.FromColorIndex(ColorMethod.ByAci, 1);
+            transaction.Commit();
+        }
+
+        [CommandMethod("DSGETENTITY")]
+        public static void GetEntity()
+        {
+            Transaction transaction = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction();
+            PromptStatus status = new PromptStatus();
+            Entity entity = DrawingSpace.GetEntity("Select object:", transaction, ref status);
+
+            // The status lets us know if the user actually selected something or cancelled.
+            if (status == PromptStatus.OK)
+            {
+                entity.Color = Color.FromColorIndex(ColorMethod.ByAci, 2);
+                transaction.Commit();
+            }
+            else
+            {
+                transaction.Dispose();
+            }
+        }
+
+        [CommandMethod("DSGETSELECTION")]
+        public static void GetSelection()
+        {
+            Transaction transaction = HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction();
+            PromptStatus status = new PromptStatus();
+            DBObjectCollection selection = DrawingSpace.GetSelection("Select objects:", transaction, ref status);
+
+            // Another way of dealing with the canceling of the operation or empty selection.
+            if (status != PromptStatus.OK)
+            {
+                transaction.Dispose();
+                return;
+            }
+
+            foreach (Entity entity in selection)
+            {
+                entity.Color = Color.FromColorIndex(ColorMethod.ByAci, 3);
+            }
+
+            transaction.Commit();
+        }
+    }
+}
