a214d6eecd8e9c58e9ed28b61515216a78d00439
Ahmed Félix <ahmed.felix@gmail.com>
2012-12-03 21:25:16 -0800

    Added GetEntityByHandle method. Added open mode, open erased objects, and force open on locked layer overloads.

--- DrawingSpace/DrawingSpace.cs
+++ DrawingSpace/DrawingSpace.cs
@@ -7,17 +7,17 @@ namespace DrawingSpace
 {
     static class DrawingSpace
     {
-
         /// <summary>
         /// Adds an entity to the Model space or current layout.
         /// </summary>
-        /// <param name="entity"></param>
         public static void AddEntity(Entity entity)
         {
             Database database = HostApplicationServices.WorkingDatabase;
             Transaction transaction = database.TransactionManager.StartTransaction();
-            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId, OpenMode.ForRead);
-            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId,
+                OpenMode.ForRead);
+            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId,
+                OpenMode.ForWrite);
 
             try
             {
@@ -38,17 +38,18 @@ namespace DrawingSpace
             }
         }
 
+
         /// <summary>
         /// Adds an entity to the Model space or current layout, keeping the transaction open
         /// so the entity can continue being used.
         /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="transaction"></param>
         public static void AddEntity(Entity entity, Transaction transaction)
         {
             Database database = HostApplicationServices.WorkingDatabase;
-            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId, OpenMode.ForRead);
-            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+            BlockTable blockTable = (BlockTable)transaction.GetObject(database.BlockTableId,
+                OpenMode.ForRead);
+            BlockTableRecord record = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId,
+                OpenMode.ForWrite);
 
             try
             {
@@ -63,14 +64,37 @@ namespace DrawingSpace
 
         }
 
+
         /// <summary>
         /// Prompts the user to select a single entity from the drawing.
         /// </summary>
         /// <param name="prompt">Message to display in the command line.</param>
-        /// <param name="transaction"></param>
-        /// <param name="status"></param>
-        /// <returns></returns>
-        public static Entity GetEntity(String prompt, Transaction transaction, ref PromptStatus status)
+        public static Entity GetEntity(String prompt, Transaction transaction, ref PromptStatus status,
+            OpenMode mode)
+        {
+            // The defaults for opening erased or locked objects are both false.
+            return GetEntity(prompt, transaction, ref status, mode, false, false);
+        }
+
+
+        /// <summary>
+        /// Prompts the user to select a single entity from the drawing.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        public static Entity GetEntity(String prompt, Transaction transaction, ref PromptStatus status,
+            OpenMode mode, bool openErased)
+        {
+            // The default for opening objects in a locked layer is false.
+            return GetEntity(prompt, transaction, ref status, mode, openErased, false);
+        }
+
+
+        /// <summary>
+        /// Prompts the user to select a single entity from the drawing.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        public static Entity GetEntity(String prompt, Transaction transaction, ref PromptStatus status,
+            OpenMode mode, bool openErased, bool forceOpenOnLockedLayer)
         {
             PromptEntityOptions options = new PromptEntityOptions(System.Environment.NewLine + prompt);
             PromptEntityResult result;
@@ -83,20 +107,92 @@ namespace DrawingSpace
 
             if (status == PromptStatus.OK)
             {
-                entity = (Entity)transaction.GetObject(result.ObjectId, OpenMode.ForWrite);
+                entity = (Entity)transaction.GetObject(result.ObjectId, mode, openErased,
+                    forceOpenOnLockedLayer);
             }
 
             return entity;
         }
 
+
+        /// <summary>
+        /// Gets an entity from a handle value.
+        /// </summary>
+        /// <returns>The entity that is identified in the drawing by the specified handle.</returns>
+        public static Entity GetEntityByHandle(String handle, Transaction transaction, OpenMode mode)
+        {
+
+            // The defaults for opening erased or locked objects are both false.
+            return GetEntityByHandle(handle, transaction, mode, false, false);
+        }
+
+
+        /// <summary>
+        /// Gets an entity from a handle value.
+        /// </summary>
+        /// <returns>The entity that is identified in the drawing by the specified handle.</returns>
+        public static Entity GetEntityByHandle(String handle, Transaction transaction,
+            OpenMode mode, bool openErased)
+        {
+            // The default for opening objects in a locked layer is false.
+            return GetEntityByHandle(handle, transaction, mode, openErased, false);
+        }
+
+
+        /// <summary>
+        /// Gets an entity from a handle value.
+        /// </summary>
+        /// <returns>The entity that is identified in the drawing by the specified handle.</returns>
+        public static Entity GetEntityByHandle(String handle, Transaction transaction,
+            OpenMode mode, bool openErased, bool forceOpenOnLockedLayer)
+        {
+            Database database = HostApplicationServices.WorkingDatabase;
+            ObjectId id = database.GetObjectId(false, new Handle(Convert.ToInt64(handle, 16)), 0);
+            Entity entity;
+
+            try
+            {
+                entity = (Entity)transaction.GetObject(id, mode, openErased, forceOpenOnLockedLayer);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return entity;
+        }
+
+
         /// <summary>
         /// Prompts the user to do a selection of one or more objects.
         /// </summary>
         /// <param name="prompt">Message to display in the command line.</param>
-        /// <param name="transaction"></param>
-        /// <param name="status"></param>
-        /// <returns></returns>
-        public static DBObjectCollection GetSelection(String prompt, Transaction transaction, ref PromptStatus status)
+        public static DBObjectCollection GetSelection(String prompt, Transaction transaction,
+            ref PromptStatus status, OpenMode mode)
+        {
+            // The defaults for opening erased or locked objects are both false.
+            return GetSelection(prompt, transaction, ref status, mode, false, false);
+        }
+
+
+        /// <summary>
+        /// Prompts the user to do a selection of one or more objects.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        public static DBObjectCollection GetSelection(String prompt, Transaction transaction,
+            ref PromptStatus status, OpenMode mode, bool openErased)
+        {
+            // The default for opening objects in a locked layer is false.
+            return GetSelection(prompt, transaction, ref status, mode, openErased, false);
+        }
+
+
+        /// <summary>
+        /// Prompts the user to do a selection of one or more objects.
+        /// </summary>
+        /// <param name="prompt">Message to display in the command line.</param>
+        public static DBObjectCollection GetSelection(String prompt, Transaction transaction,
+            ref PromptStatus status, OpenMode mode, bool openErased, bool forceOpenOnLockedLayer)
         {
             PromptSelectionOptions options = new PromptSelectionOptions();
             PromptSelectionResult result;
@@ -119,7 +215,7 @@ namespace DrawingSpace
 
                 foreach (ObjectId id in idArray)
                 {
-                    dbObject = transaction.GetObject(id, OpenMode.ForWrite, false, true);
+                    dbObject = transaction.GetObject(id, mode, openErased, forceOpenOnLockedLayer);
                     objectsSelected.Add(dbObject);
                 }
             }
