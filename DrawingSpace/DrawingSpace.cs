7642998df45ebe89422e95d0535597317b096f73
Ahmed F�lix <ahmed.felix@gmail.com>
2013-01-16 20:00:12 -0800

    Refactor: rename prompt to message for consistency with AutoCAD API

--- DrawingSpace/DrawingSpace.cs
+++ DrawingSpace/DrawingSpace.cs
@@ -117,26 +117,26 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to input a double.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <param name="allowNone">If false, the user is forced to enter a number or cancel.</param>
         /// <returns>The double the user wrote. Any other operation will return 0.</returns>
-        public static double GetDouble(string prompt, ref PromptStatus status, bool allowNone)
+        public static double GetDouble(string message, ref PromptStatus status, bool allowNone)
         {
             // Default value is 0.
-            return GetDouble(prompt, ref status, allowNone, 0.0);
+            return GetDouble(message, ref status, allowNone, 0.0);
         }
 
 
         /// <summary>
         /// Prompts the user to input a double.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <param name="allowNone">If false, the user is forced to enter a number or cancel.</param>
         /// <returns>The double the user wrote. Any other operation will return the default value.</returns>
-        public static double GetDouble(string prompt, ref PromptStatus status, bool allowNone,
+        public static double GetDouble(string message, ref PromptStatus status, bool allowNone,
             double defaultValue)
         {
-            PromptDoubleOptions options = new PromptDoubleOptions(System.Environment.NewLine + prompt);
+            PromptDoubleOptions options = new PromptDoubleOptions(System.Environment.NewLine + message);
             options.AllowNone = allowNone;
             options.DefaultValue = defaultValue;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
@@ -151,35 +151,35 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to select a single entity from the drawing.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static Entity GetEntity(string prompt, Transaction transaction, ref PromptStatus status,
+        /// <param name="message">Message to display in the command line.</param>
+        public static Entity GetEntity(string message, Transaction transaction, ref PromptStatus status,
             OpenMode mode)
         {
             // The defaults for opening erased or locked objects are both false.
-            return GetEntity(prompt, transaction, ref status, mode, false, false);
+            return GetEntity(message, transaction, ref status, mode, false, false);
         }
 
 
         /// <summary>
         /// Prompts the user to select a single entity from the drawing.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static Entity GetEntity(string prompt, Transaction transaction, ref PromptStatus status,
+        /// <param name="message">Message to display in the command line.</param>
+        public static Entity GetEntity(string message, Transaction transaction, ref PromptStatus status,
             OpenMode mode, bool openErased)
         {
             // The default for opening objects in a locked layer is false.
-            return GetEntity(prompt, transaction, ref status, mode, openErased, false);
+            return GetEntity(message, transaction, ref status, mode, openErased, false);
         }
 
 
         /// <summary>
         /// Prompts the user to select a single entity from the drawing.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static Entity GetEntity(string prompt, Transaction transaction, ref PromptStatus status,
+        /// <param name="message">Message to display in the command line.</param>
+        public static Entity GetEntity(string message, Transaction transaction, ref PromptStatus status,
             OpenMode mode, bool openErased, bool forceOpenOnLockedLayer)
         {
-            PromptEntityOptions options = new PromptEntityOptions(System.Environment.NewLine + prompt);
+            PromptEntityOptions options = new PromptEntityOptions(System.Environment.NewLine + message);
             PromptEntityResult result;
             Entity entity = null;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
@@ -248,26 +248,26 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to input a double.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <param name="allowNone">If false, the user is forced to enter a number or cancel.</param>
         /// <returns>The integer the user wrote. Any other operation will return 0.</returns>
-        public static int GetInteger(string prompt, ref PromptStatus status, bool allowNone)
+        public static int GetInteger(string message, ref PromptStatus status, bool allowNone)
         {
             // Default value is 0.
-            return GetInteger(prompt, ref status, allowNone, 0);
+            return GetInteger(message, ref status, allowNone, 0);
         }
 
 
         /// <summary>
         /// Prompts the user to input a double.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <param name="allowNone">If false, the user is forced to enter a number or cancel.</param>
         /// <returns>The integer the user wrote. Any other operation will return the default value.</returns>
-        public static int GetInteger(string prompt, ref PromptStatus status, bool allowNone,
+        public static int GetInteger(string message, ref PromptStatus status, bool allowNone,
             int defaultValue)
         {
-            PromptIntegerOptions options = new PromptIntegerOptions(System.Environment.NewLine + prompt);
+            PromptIntegerOptions options = new PromptIntegerOptions(System.Environment.NewLine + message);
             options.AllowNone = allowNone;
             options.DefaultValue = defaultValue;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
@@ -281,14 +281,14 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to select a keyword from a list.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <param name="keywords">List of keywords from which the user will choose.</param>
         /// <returns>The keyword selected by the user.</returns>
         /// <remarks>The keywords must be single words. If a keyword includes spaces, 
         /// only the first word will be returned.</remarks>
-        public static string GetKeyword(string prompt, ref PromptStatus status, string[] keywords)
+        public static string GetKeyword(string message, ref PromptStatus status, string[] keywords)
         {
-            PromptKeywordOptions options = new PromptKeywordOptions(prompt);
+            PromptKeywordOptions options = new PromptKeywordOptions(message);
             PromptResult result;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
             
@@ -307,10 +307,10 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to select a point in the drawing.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static Point3d GetPoint3d(string prompt, ref PromptStatus status)
+        /// <param name="message">Message to display in the command line.</param>
+        public static Point3d GetPoint3d(string message, ref PromptStatus status)
         {
-            PromptPointOptions options = new PromptPointOptions(prompt);
+            PromptPointOptions options = new PromptPointOptions(message);
             PromptPointResult result;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
 
@@ -325,32 +325,32 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to do a selection of one or more objects.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static DBObjectCollection GetSelection(string prompt, Transaction transaction,
+        /// <param name="message">Message to display in the command line.</param>
+        public static DBObjectCollection GetSelection(string message, Transaction transaction,
             ref PromptStatus status, OpenMode mode)
         {
             // The defaults for opening erased or locked objects are both false.
-            return GetSelection(prompt, transaction, ref status, mode, false, false);
+            return GetSelection(message, transaction, ref status, mode, false, false);
         }
 
 
         /// <summary>
         /// Prompts the user to do a selection of one or more objects.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static DBObjectCollection GetSelection(string prompt, Transaction transaction,
+        /// <param name="message">Message to display in the command line.</param>
+        public static DBObjectCollection GetSelection(string message, Transaction transaction,
             ref PromptStatus status, OpenMode mode, bool openErased)
         {
             // The default for opening objects in a locked layer is false.
-            return GetSelection(prompt, transaction, ref status, mode, openErased, false);
+            return GetSelection(message, transaction, ref status, mode, openErased, false);
         }
 
 
         /// <summary>
         /// Prompts the user to do a selection of one or more objects.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
-        public static DBObjectCollection GetSelection(string prompt, Transaction transaction,
+        /// <param name="message">Message to display in the command line.</param>
+        public static DBObjectCollection GetSelection(string message, Transaction transaction,
             ref PromptStatus status, OpenMode mode, bool openErased, bool forceOpenOnLockedLayer)
         {
             PromptSelectionOptions options = new PromptSelectionOptions();
@@ -359,7 +359,7 @@ namespace DrawingSpace
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
             DBObjectCollection objectsSelected = new DBObjectCollection();
 
-            options.MessageForAdding = System.Environment.NewLine + prompt;
+            options.MessageForAdding = System.Environment.NewLine + message;
             result = command.GetSelection(options);
             status = result.Status;
 
@@ -386,25 +386,25 @@ namespace DrawingSpace
         /// <summary>
         /// Prompts the user to input a string.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <returns>The string the user wrote. Any other operation will return an empty string.</returns>
         /// <remarks>If the user doesn't select anything, status will still return OK, not None.</remarks>
-        public static string GetString(string prompt, ref PromptStatus status, bool allowSpaces)
+        public static string GetString(string message, ref PromptStatus status, bool allowSpaces)
         {
-            return GetString(prompt, ref status, allowSpaces, null);
+            return GetString(message, ref status, allowSpaces, null);
         }
 
 
         /// <summary>
         /// Prompts the user to input a string.
         /// </summary>
-        /// <param name="prompt">Message to display in the command line.</param>
+        /// <param name="message">Message to display in the command line.</param>
         /// <returns>The string the user wrote or the default value if nothing was written.</returns>
         /// <remarks>If the user doesn't select anything, status will still return OK, not None.</remarks>
-        public static string GetString(string prompt, ref PromptStatus status, bool allowSpaces,
+        public static string GetString(string message, ref PromptStatus status, bool allowSpaces,
             string defaultValue)
         {
-            PromptStringOptions options = new PromptStringOptions(System.Environment.NewLine + prompt);
+            PromptStringOptions options = new PromptStringOptions(System.Environment.NewLine + message);
             options.AllowSpaces = allowSpaces;
             options.DefaultValue = defaultValue;
             Editor command = Application.DocumentManager.MdiActiveDocument.Editor;
